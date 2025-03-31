e6259e4ad4287591b83e821c36cf8bb1a85c0fe0
YashKharcheHS <yash.kharche@thehotspring.com>
2025-04-01 02:15:56 +0530

    Save changes before installing OpenZeppelin2

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-
+import {ERC20} from "counter/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 contract Counter {
     uint256 public number;
 
