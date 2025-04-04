8f68b3a943d6053409dd02ac680845eb234b7bab
YashKharcheHS <yash.kharche@thehotspring.com>
2025-04-04 17:22:04 +0530

    setup foundry

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-import {ERC20} from "counter/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {LibString} from "@solady-utils/LibString.sol";
+import {ERC20} from "@openzeppelin-contracts/token/ERC20/ERC20.sol";
+
 contract Counter {
     uint256 public number;
 
